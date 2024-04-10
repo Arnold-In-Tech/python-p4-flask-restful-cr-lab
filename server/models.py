e8e6d828d66d182d732e96c49baac524109712e9
Arnold-In-Tech <g16a7782@gmail.com>
2024-04-11 01:59:08 +0300

    completed lab

--- server/models.py
+++ server/models.py
@@ -7,3 +7,12 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Numeric(precision=10, scale=2)) 
+    # Precision is the number of digits in a number. 
+    # Scale is the number of digits to the right of the decimal point in a number.
+    # E.g.  123.45 has a precision of 5 and a scale of 2.  
+
+    def __repr__(self):
+        return f"<Plant {self.name} , {self.price}>"
